5a70f1b4a0362516ed9d94caa277ba382bf97e48
mafparejo <45272983+mafparejo@users.noreply.github.com>
2019-02-22 14:30:06 +0100

    Create ColegioPrivado.java

---
+++ ColegioPrivado.java
@@ -0,0 +1,20 @@
+package proyecto_2;
+
+public class ColegioPrivado {
+
+	public int getPago(int i) {
+		// TODO Auto-generated method stub
+		return 300;
+	}
+
+	public String getDireccion() {
+		// TODO Auto-generated method stub
+		return "OTRO";
+	}
+	
+	public boolean getComprobar() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+}
